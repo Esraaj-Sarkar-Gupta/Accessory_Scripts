89125bb19a19d95f31c87e2009302e0cbf4727b5
Esraaj Sarkar Gupta <tsampi.24@gmail.com>
2024-07-04 16:01:31 +0530

    Add files via upload

---
+++ loggerFunction.py
@@ -0,0 +1,7 @@
+import time as tm
+
+def log(text):
+	T = tm.localtime()
+	with open(f"Logs_{T[7]}_{T[3]}-{T[4]}-{T[5]}.txt" , 'a') as log_file:
+		log_file.write(f"\n{T[7]} - {T[3]}:{T[4]}:{T[5]} > {text}")
+	print(f"{T[3]}:{T[4]}:{T[5]} > {text}")

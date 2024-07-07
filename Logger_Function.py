f105f73d11ffcdec2fabd5ffd3e2399bbb71a398
Esraaj Sarkar Gupta <tsampi.24@gmail.com>
2024-07-07 20:41:23 +0530

    Update Logger_Function.py

--- Logger_Function.py
+++ Logger_Function.py
@@ -5,3 +5,10 @@ def log(text):
 	with open(f"Logs_{T[7]}_{T[3]}-{T[4]}-{T[5]}.txt" , 'a') as log_file:
 		log_file.write(f"\n{T[7]} - {T[3]}:{T[4]}:{T[5]} > {text}")
 	print(f"{T[3]}:{T[4]}:{T[5]} > {text}")
+
+
+def backend_log(text):
+	T = tm.localtime()
+	with open(f"Logs_{T[7]}_{T[3]}-{T[4]}-{T[5]}.txt" , 'a') as log_file:
+		log_file.write(f"\n{T[7]} - {T[3]}:{T[4]}:{T[5]} > {text}")
+

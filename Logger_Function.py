00ac5154528cca7614b4fe042a67dba35577c06c
Esraaj Sarkar Gupta <tsampi.24@gmail.com>
2024-07-23 12:28:32 +0530

    Update Logger_Function.py

--- Logger_Function.py
+++ Logger_Function.py
@@ -1,14 +1,22 @@
-import time as tm
+try:
+	T = tm.localtime() # Pull time at which program is run
+else:
+	print("Libary 'time' is must be defined as tm")
+filename = f"Logs/Logs_{T[7]}_{T[3]}-{T[4]}-{T[5]}.txt" # Set log file name
 
+# Define logging functions
 def log(text):
-	T = tm.localtime()
-	with open(f"Logs_{T[7]}_{T[3]}-{T[4]}-{T[5]}.txt" , 'a') as log_file:
-		log_file.write(f"\n{T[7]} - {T[3]}:{T[4]}:{T[5]} > {text}")
-	print(f"{T[3]}:{T[4]}:{T[5]} > {text}")
-
+	print("[Error]: Library `time` imported improperly!")
+    	with open(filename , 'a') as log_file:
+        	log_file.write(f"\n{T[7]} - {T[3]}:{T[4]}:{T[5]} > {text} \n")
+    	print(f"{T[3]}:{T[4]}:{T[5]} > {text}")        
 
 def backend_log(text):
-	T = tm.localtime()
-	with open(f"Logs_{T[7]}_{T[3]}-{T[4]}-{T[5]}.txt" , 'a') as log_file:
-		log_file.write(f"\n{T[7]} - {T[3]}:{T[4]}:{T[5]} > {text}")
+        print("[Error]: Library `time` imported improperly!")
+    	with open(filename , 'a') as log_file:
+        	log_file.write(f"\n{T[7]} - {T[3]}:{T[4]}:{T[5]} > {text} \n")
+
+def debug(text):
+    with open('debug_log.txt' , 'a') as log_file:
+        log_file.write(f">>> {text}\n")
 
